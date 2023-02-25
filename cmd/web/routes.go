d156975f1bde56d09f6028ab8e9676a03828ef81
Jamshid K <jamshid.k@zeeve.io>
2023-02-25 14:47:34 +0530

    custom middleware and csrfHandker

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,22 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jamshu/go_web_app/pkg/config"
 	"github.com/jamshu/go_web_app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	mux.Get("/contact", http.HandlerFunc(handlers.Repo.Contact))
-	
+	mux := chi.NewRouter()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/contact", http.HandlerFunc(handlers.Repo.Contact))
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Logger)
+	mux.Use(NoSurf)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/contact", handlers.Repo.Contact)
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/jamshu/go_web_app
 
 go 1.20
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.0.8
+
+require github.com/justinas/nosurf v1.1.1 // indirect
